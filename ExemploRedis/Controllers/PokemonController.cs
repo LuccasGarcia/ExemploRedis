1da74e3791c13b6d98cb9682720480b436070c3a
Luccas Garcia <34176699+LuccasGarcia@users.noreply.github.com>
2022-03-16 16:51:35 -0300

    Commit envio

---
+++ ExemploRedis/Controllers/PokemonController.cs
@@ -0,0 +1,39 @@
+﻿using ExemploRedis.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ExemploRedis.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private readonly IPokemonService _pokemonService;
+        private readonly ICacheService<Pokemon> _pokemonCacheService;
+
+        public PokemonController(IPokemonService pokemonService, ICacheService<Pokemon> pokemonCacheService)
+        {
+            _pokemonService = pokemonService;
+            _pokemonCacheService = pokemonCacheService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            while (id < 150)
+            {
+                Pokemon pokemon = await _pokemonCacheService.Get(id);
+                if (pokemon is null)
+                {
+                    pokemon = await _pokemonService.GetPokemon(id);
+                    await _pokemonCacheService.Set(pokemon);
+                }
+                //return Ok(pokemon);
+                id++;
+            }
+            return null;
+        }
+
+
+    }
+}
---
+++ ExemploRedis/Extensions/DistributedCacheExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExemploRedis.Extensions
+{
+    public static class DistributedCacheExtension
+    {
+        public static IServiceCollection AddDistributedCache(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDistributedRedisCache(options =>
+            {
+                options.Configuration = configuration.GetConnectionString("Redis");
+                options.InstanceName = configuration["Redis:InstanceName"];
+            });
+            return services;
+        }
+    }
+}
